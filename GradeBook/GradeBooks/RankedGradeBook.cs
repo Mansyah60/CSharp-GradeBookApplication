79526be7123556df0f84e83b4e5e3cd7e20a7c79
Mansyah60 <mansyah_60@hotmail.com>
2018-03-10 16:53:36 +0100

    Add Override of GetLetterGrade
    Override GetLetterGrade in RankedGradeBook.

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,4 +1,6 @@
 ﻿using GradeBook.Enums;
+using System;
+using System.Linq;
 
 namespace GradeBook.GradeBooks
 {
@@ -7,6 +9,34 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) : base(name)
         {
             Type = GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+                throw new InvalidOperationException("Ranked grading requires at least 5 students.");
+
+            // Position per 20% of the students for determining the threshold pe Rank.
+            var threshold = Convert.ToInt32(Math.Ceiling((Students.Count * 0.2)));
+
+            // Select parameter AverageGrade from the list of Student in ascending order
+            var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
+
+            // The poisition of last 20% of the Students
+            if (grades[threshold - 1] <= averageGrade)
+                return 'A';
+            // The position of the last 40% of the Students
+            else if (grades[threshold * 2 - 1] <= averageGrade)
+                return 'B';
+            // The position of the last 60% of the Students
+            else if (grades[threshold * 3 - 1] <= averageGrade)
+                return 'C';
+            // The position of the last 80% of the Students
+            else if (grades[threshold * 4 - 1] <= averageGrade)
+                return 'D';
+            else 
+                return 'F';
+
         }
     }
 }
